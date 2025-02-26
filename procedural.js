c3e5925f0d5a83123998f60433b5c13505c72965
mrablfz <abolfaz88888888@gmail.com>
2025-02-26 04:29:22 -0800

    [Procedural]: make instructions!

---
+++ procedural.js
@@ -0,0 +1,9 @@
+function solution() {
+    // Procedural instructions step by step:
+
+    // 1. read the file
+    
+    // 2. parse the file
+    // 3. calculate the sum of cards
+    // 4. print output: sum of cards
+}
