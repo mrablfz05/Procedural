b7360826b1d1d8036dce6f876218c9a3008b3fcc
mrablfz <abolfaz88888888@gmail.com>
2025-02-26 08:13:03 -0800

    [Split]: splited more to be clean readable object!

--- procedural.js
+++ procedural.js
@@ -3,9 +3,20 @@ import fs from "fs"
 const solution = () => {
     // Procedural instructions step by step:
 
-    // 1. read the file
-    let readedData = ''
+    class Card {
+        name;
+        winningNumbers;
+        allNumbers;
 
+        constructor(name, winningNumbers, allNumbers) {
+            this.name = name
+            this.winningNumbers = winningNumbers
+            this.allNumbers = allNumbers
+        }
+    }
+
+
+    let readedData = ''
     
     const processFile = () => {
         fs.readFile('problem.txt', 'utf-8', (err, data) => {
@@ -13,33 +24,53 @@ const solution = () => {
                 console.log('Error while reading file!')
             } else {
                 readedData = data
-                // console.log(readedData)
                 splitData(data)
+                const cards = parseCards(data)
+                console.log(cards) 
             }
         })
     }
     processFile()
+
+    const parseCards = (data) => {
+      const splitResult = data.split('\n').map((line) => line.trim().split('|'))
+      let cards = []
+
+      splitResult.forEach((line) => {
+        if (line.length < 2) return // Skip invalid lines
+
+        let [nameAndWinning, allNumbers] = line
+
+        // Extract name (Example: "Card 1")
+        let nameMatch = nameAndWinning.match(/(Card \d+)/) // Extract "Card X"
+        let name = nameMatch ? nameMatch[0] : 'Unknown Card' // Ensure we get full "Card X"
+
+        // Remove "Card X" and extra symbols like ":" from winning numbers
+        let winningNumbers = nameAndWinning
+          .replace(name, '')
+          .replace(':', '')
+          .trim()
+
+        // Convert numbers into an array (remove extra spaces and split by space)
+        const allNumbersArray = allNumbers.trim().split(/\s+/)
+        const winningNumbersArray = winningNumbers
+          .split(/\s+/)
+          .filter((num) => allNumbersArray.includes(num))
+
+        // Create a Card object and push it to the array
+        cards.push(new Card(name, winningNumbersArray, allNumbersArray))
+      })
+
+      return cards
+    }
     
-    // 2. parse the file
+
     
     const splitData = (data) => {
         const splitResult = data.split('\n')
-        console.log('Split Data:', splitResult)
+        const furtherSplit = splitResult.map(line => line.split(":"))
+        console.log(furtherSplit)
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
