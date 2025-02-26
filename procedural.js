9a15fc47df1ba5e455e2285bc0b992bab17a8cfd
mrablfz <abolfaz88888888@gmail.com>
2025-02-26 05:38:30 -0800

    [Split]: splited data in an array but we need split more!

--- procedural.js
+++ procedural.js
@@ -1,19 +1,48 @@
 import fs from "fs"
 
-function solution() {
+const solution = () => {
     // Procedural instructions step by step:
 
     // 1. read the file
+    let readedData = ''
 
-
-    fs.readFile('problem.txt', 'utf-8', (err, data) => {
-      if (err) {
-        console.log('Error while reading file!')
-      } else {
-        console.log('File Content: ', data)
-      }
-    })
+    
+    const processFile = () => {
+        fs.readFile('problem.txt', 'utf-8', (err, data) => {
+            if (err) {
+                console.log('Error while reading file!')
+            } else {
+                readedData = data
+                // console.log(readedData)
+                splitData(data)
+            }
+        })
+    }
+    processFile()
+    
     // 2. parse the file
+    
+    const splitData = (data) => {
+        const splitResult = data.split('\n')
+        console.log('Split Data:', splitResult)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // 3. calculate the sum of cards
     // 4. print output: sum of cards
 }
