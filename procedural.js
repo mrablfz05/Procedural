ccc2c3601bda95d7fb9cddb46b6f904faaa05f90
mrablfz <abolfaz88888888@gmail.com>
2025-02-26 04:52:43 -0800

    [Read]: readed file problem.txt in correct format!

--- procedural.js
+++ procedural.js
@@ -1,9 +1,21 @@
+import fs from "fs"
+
 function solution() {
     // Procedural instructions step by step:
 
     // 1. read the file
-    
+
+
+    fs.readFile('problem.txt', 'utf-8', (err, data) => {
+      if (err) {
+        console.log('Error while reading file!')
+      } else {
+        console.log('File Content: ', data)
+      }
+    })
     // 2. parse the file
     // 3. calculate the sum of cards
     // 4. print output: sum of cards
 }
+
+solution()
